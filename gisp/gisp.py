dac89c3d64f38a632833530f3e3bc6785b465d47
yangsheng <wu0306109@gmail.com>
2022-11-24 13:58:05 +0800

    Define function APIs.

---
+++ gisp/gisp.py
@@ -0,0 +1,94 @@
+from typing import Callable, List, NamedTuple, Tuple
+
+from pandas import DataFrame
+
+
+class Pattern(NamedTuple):
+
+    # frequent interval-extended sequence,
+    # which is a list of (itemized_interval, item)
+    sequence: List[Tuple[int, str]]
+    support: int  # number of the pattern occurrence
+
+
+class Gisp:
+    # A class maintaining settings for
+    # Generalized Interval-extended Sequence Pattern mining
+    # 
+    # There are two terms of database used in this class:
+    # ISDB (interval-extended sequence database) and
+    # PDB (Postfix database) which is an extention of ISDB.
+    #
+    # ISDB with columns 'sid': the ID of the sequence, 'item': the item itself,
+    # and 'interval': the occurence of the item. ISDB should be sorted by 
+    # sid > interval > item ascendingly. A sequence for example
+    # [(0, a), (86400m abc), (259200, ac)] would be represented as follow:
+    #
+    #   sid     interval     item
+    # 
+    #   0       0            a
+    #   0       86400        a
+    #   0       86400        b
+    #   0       86400        c
+    #   0       259200       a
+    #   0       259200       c
+    #
+    # PDB is an extention of ISDB with 2 mote columns 'pid' is the ID of the 
+    # postfix and 'whole_interval' is the interval from the head item. The a- 
+    # projected interval-extended sequence database represented in PDB is a the
+    # collection of postfixes regard to a, where a is an interval-extended 
+    # sequence.
+
+    def __init__(
+            self, itemize: Callable[[int], int], min_support: int,
+            min_interval: int = None, max_interval: int = None,
+            min_whole_interval: int = None, max_whole_interval: int = None
+    ) -> None:
+        pass
+
+    @staticmethod
+    def transform(sequences: List[Tuple[int, List[str]]]) -> DataFrame:
+        """Transform sequences into DataFrame (ISDB) for mining."""
+        pass
+
+    def mine(self, isdb: DataFrame) -> List[Pattern]:
+        """Driver function to run the algorithm on the given database."""
+        pass
+
+    def mine_subpatterns(self, pdb: DataFrame) -> List[Pattern]:
+        """Perform level 2 or later projection to mine subpatterns recursively.
+
+        Args:
+            projected_db: The a- projected interval-extended sequence database 
+                is a the collection of postfixes regard to a, where a be an 
+                interval-extended sequence. Each item in the database is a list
+                of postfixes for the original sequence.
+        """
+        pass
+
+
+def mine(
+    sequences: List[Tuple[int, List[str]]], itemize: Callable[[int], int],
+    min_support: int, min_interval: int = None, max_interval: int = None,
+    min_whole_interval: int = None, max_whole_interval: int = None,
+) -> List[Pattern]:
+    """Mine frequent interval-extended sequences.
+
+    Args:
+        sequences: Interval-extended sequences, 
+            where each sequence is a list of (interval, items).
+        itemize: Converting function from interval to pseudo counts.
+        min_support: Minimal number of occurrence for resulting patterns.
+        min_interval: Minimum interval between any two adjacent items.
+        max_interval: Maximum interval between any two adjacent items.
+        min_whole_interval: Minimum interval between 
+            the head and the tail of the sequence.
+        max_whole_interval: Maximum interval between
+            the head and the tail of the sequence.
+
+    Returns:
+        List of Pattern(sequence, support), 
+        where sequence is a list of (itemized_interval, item),
+        and support is the number of the pattern occurrence.
+    """
+    pass
