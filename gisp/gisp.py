ef3f7c10dfd05041d73ae60c46171f8c571f9560
yangsheng <wu0306109@gmail.com>
2022-11-25 22:25:40 +0800

    Implement `Gisp.mine_subpatterns()`.

--- gisp/gisp.py
+++ gisp/gisp.py
@@ -1,6 +1,6 @@
 from typing import Callable, List, NamedTuple, Tuple
 
-from pandas import DataFrame
+from pandas import DataFrame, concat
 
 
 class Pattern(NamedTuple):
@@ -14,18 +14,18 @@ class Pattern(NamedTuple):
 class Gisp:
     # A class maintaining settings for
     # Generalized Interval-extended Sequence Pattern mining
-    # 
+    #
     # There are two terms of database used in this class:
     # ISDB (interval-extended sequence database) and
     # PDB (Postfix database) which is an extention of ISDB.
     #
     # ISDB with columns 'sid': the ID of the sequence, 'item': the item itself,
-    # and 'interval': the occurence of the item. ISDB should be sorted by 
+    # and 'interval': the occurence of the item. ISDB should be sorted by
     # sid > interval > item ascendingly. A sequence for example
     # [(0, a), (86400m abc), (259200, ac)] would be represented as follow:
     #
     #   sid     item    interval
-    # 
+    #
     #   0       a       0
     #   0       a       86400
     #   0       b       86400
@@ -33,30 +33,36 @@ class Gisp:
     #   0       a       259200
     #   0       c       259200
     #
-    # PDB is an extention of ISDB with 2 mote columns 'pid' is the ID of the 
-    # postfix and 'whole_interval' is the interval from the head item. The a- 
+    # PDB is an extention of ISDB with 2 mote columns 'pid' is the ID of the
+    # postfix and 'whole_interval' is the interval from the head item. The a-
     # projected interval-extended sequence database represented in PDB is a the
-    # collection of postfixes regard to a, where a is an interval-extended 
+    # collection of postfixes regard to a, where a is an interval-extended
     # sequence.
 
     def __init__(
             self, itemize: Callable[[int], int], min_support: int,
-            min_interval: int = None, max_interval: int = None,
-            min_whole_interval: int = None, max_whole_interval: int = None
+            min_interval: int, max_interval: int,
+            min_whole_interval: int, max_whole_interval: int
     ) -> None:
-        pass
+        self._itemize = itemize
+        self._min_support = min_support
+        self._min_interval = min_interval
+        self._max_interval = max_interval
+        self._min_whole_interval = min_whole_interval
+        self._max_whole_interval = max_whole_interval
 
     @staticmethod
     def transform(sequences: List[Tuple[int, List[str]]]) -> DataFrame:
         """Transform sequences into DataFrame (ISDB) for mining."""
-        
+
         def yield_item_rows() -> Tuple[int, str, int]:
             for sid, sequence in enumerate(sequences):
                 for interval, items in sequence:
                     for item in items:
                         yield sid, item, interval
 
-        isdb = DataFrame(yield_item_rows(), columns=['sid', 'item', 'interval'])
+        isdb = DataFrame(yield_item_rows(), columns=[
+                         'sid', 'item', 'interval'])
         isdb.sort_values(by=['sid', 'interval', 'item'], inplace=True)
         return isdb
 
@@ -73,7 +79,43 @@ class Gisp:
                 interval-extended sequence. Each item in the database is a list
                 of postfixes for the original sequence.
         """
-        pass
+
+        def yield_sub_pdbs(itemized_interval: int, item: str) -> DataFrame:
+            """Yield sub-PDBs of postfix projected by (itemized_interval, item).
+            """
+            matches = pdb[
+                (pdb['item'] == item)
+                & (pdb['itemized_interval'] == itemized_interval)]
+            for i, (_, pid, _, interval, _, _) in matches.iterrows():
+                sub_pdb = pdb[pdb['pid'] == pid].loc[i + 1:]
+                sub_pdb['interval'] -= interval
+                yield sub_pdb.drop(columns=['itemized_interval'])
+
+        pdb['itemized_interval'] = pdb['interval'].apply(self._itemize)
+
+        constraints = (
+            (pdb['interval'] >= self._min_interval)
+            & (pdb['interval'] <= self._max_interval)
+            & (pdb['whole_interval'] >= self._min_whole_interval)
+            & (pdb['whole_interval'] <= self._max_whole_interval))
+        counts = pdb[constraints].drop_duplicates(
+            subset=['sid', 'item', 'itemized_interval']).value_counts(
+                subset=['item', 'itemized_interval'])
+        counts = counts[counts >= self._min_support]
+
+        patterns = []
+        for (item, itemized_interval), count in counts.items():
+            patterns.append(Pattern(
+                sequence=[(itemized_interval, item)],
+                support=count))
+
+            child_pdb = concat(yield_sub_pdbs(itemized_interval, item))
+            subpatterns = self.mine_subpatterns(child_pdb)
+
+            for pattern in subpatterns:
+                pattern.sequence.insert(0, (itemized_interval, item))
+            patterns.extend(subpatterns)
+        return patterns
 
 
 def mine(
--- tests/test_gisp.py
+++ tests/test_gisp.py
@@ -1,19 +1,20 @@
+from math import inf
+
 from pandas import DataFrame
 from pandas.testing import assert_frame_equal
 
-from gisp.gisp import Gisp
+from gisp.gisp import Gisp, Pattern
 
 
 class TestGisp:
 
-    def test_transform(self):
+    def test_transform(self) -> None:
         sequences = [
             [(0, ['a', ]), (86400, ['a', 'b', 'c', ]), (259200, ['a', 'c', ])],
             [(0, ['a', 'd', ]), (259200, ['c', ])],
             [(0, ['a', 'e', 'f', ]), (172800, ['a', 'b', ])],
         ]
-        gisp = Gisp(itemize=None, min_support=None)
-        isdb = gisp.transform(sequences)
+        isdb = Gisp.transform(sequences)
         right = DataFrame(
             [
                 (0, 'a', 0),
@@ -35,4 +36,34 @@ class TestGisp:
         )
         assert_frame_equal(isdb, right, check_dtype=False)
 
-
+    def test_mine_subpatterns(self) -> None:
+        pdb = DataFrame(
+            [
+                (0, 0, 'a', 86400, 86400),
+                (0, 0, 'b', 86400, 86400),
+                (0, 0, 'c', 86400, 86400),
+                (0, 0, 'a', 259200, 259200),
+                (0, 0, 'c', 259200, 259200),
+                (0, 1, 'b', 0, 0),
+                (0, 1, 'c', 0, 0),
+                (0, 1, 'a', 172800, 172800),
+                (0, 1, 'c', 172800, 172800),
+                (0, 2, 'c', 0, 0),
+                (1, 3, 'd', 0, 0),
+                (1, 3, 'c', 259200, 259200),
+                (2, 4, 'e', 0, 0),
+                (2, 4, 'f', 0, 0),
+                (2, 4, 'a', 172800, 172800),
+                (2, 4, 'b', 172800, 172800),
+                (2, 5, 'b', 0, 0),
+            ],
+            columns=['sid', 'pid', 'item', 'interval', 'whole_interval'],
+        )
+        gisp = Gisp(
+            itemize=lambda t: t//86400, min_support=2, min_interval=0,
+            max_interval=172900, min_whole_interval=0, max_whole_interval=inf)
+        patterns = gisp.mine_subpatterns(pdb)
+        assert sorted(patterns) == sorted([
+            Pattern([(0, 'b')], 2),
+            Pattern([(2, 'a')], 2),
+        ])
